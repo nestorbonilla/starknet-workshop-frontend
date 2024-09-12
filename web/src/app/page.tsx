1363a0c4e6ad3ce2b2b037bf30f33082b7efc714
Nestor Bonilla <nestor.bonilla.s@gmail.com>
2024-09-11 17:58:00 -0700

    Update state of contract

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,7 +1,7 @@
 'use client';
-import { FC } from 'react';
+import { FC, useMemo, useState } from 'react';
 import dynamic from 'next/dynamic';
-import { useAccount, useBalance, useBlockNumber, useReadContract } from '@starknet-react/core';
+import { useAccount, useBalance, useBlockNumber, useContract, useReadContract, useSendTransaction, useTransactionReceipt } from '@starknet-react/core';
 import { BlockNumber } from "starknet";
 import { ABI } from "../abis/abi";
 import { type Abi } from "starknet";
@@ -34,6 +34,72 @@ const Page: FC = () => {
     refetchInterval: 1000
   });
   // Step 3 --> Read from a contract -- End
+
+  // Step 4 --> Write to a contract -- Start
+  const [amount, setAmount] = useState<number | ''>(0);
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("Form submitted with amount ", amount);
+    writeAsync();
+  };
+  const typedABI = ABI as Abi;
+  const { contract } = useContract({
+    abi: typedABI,
+    address: contractAddress,
+  });
+  const calls = useMemo(() => {
+    if (!userAddress || !contract) return [];
+    const safeAmount = amount || 0;
+    return [contract.populate("increase_balance", [safeAmount])];
+  }, [contract, userAddress, amount]);
+  const {
+    send: writeAsync,
+    data: writeData,
+    isPending: writeIsPending,
+  } = useSendTransaction({
+    calls,
+  });
+  const {
+    data: waitData,
+    status: waitStatus,
+    isLoading: waitIsLoading,
+    isError: waitIsError,
+    error: waitError
+  } = useTransactionReceipt({ hash: writeData?.transaction_hash, watch: true })
+  const handleAmountChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    setAmount(value === '' ? '' : Number(value));
+  };
+  const LoadingState = ({ message }: { message: string }) => (
+    <div className="flex items-center space-x-2">
+      <div className="animate-spin">
+        <svg className="h-5 w-5 text-gray-800" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
+        </svg>
+      </div>
+      <span>{message}</span>
+    </div>
+  );
+  const buttonContent = () => {
+    if (writeIsPending) {
+      return <LoadingState message="Send..." />;
+    }
+
+    if (waitIsLoading) {
+      return <LoadingState message="Waiting for confirmation..." />;
+    }
+
+    if (waitStatus === "error") {
+      return <LoadingState message="Transaction rejected..." />;
+    }
+
+    if (waitStatus === "success") {
+      return "Transaction confirmed";
+    }
+
+    return "Send";
+  };
+  // Step 4 --> Write to a contract -- End
 
   return (
     <div className="min-h-screen bg-gray-100 p-4 flex flex-col">
@@ -103,6 +169,60 @@ const Page: FC = () => {
             </button>
           </div> */}
           {/* Step 3 --> Read from a contract -- End */}
+
+          {/* Step 4 --> Write to a contract -- Start */}
+          <form onSubmit={handleSubmit} className="bg-white p-4 border-black border">
+            <h3 className="text-lg font-bold mb-2">Write to Contract</h3>
+            <label htmlFor="amount" className="block text-sm font-medium text-gray-700">Amount:</label>
+            <input
+              type="number"
+              id="amount"
+              value={amount}
+              onChange={handleAmountChange}
+              className="block w-full px-3 py-2 text-sm leading-6 border-black focus:outline-none focus:border-yellow-300 black-border-p"
+            />
+            <button
+              type="submit"
+              className="mt-3 border border-black text-black font-regular py-2 px-4 bg-yellow-300 hover:bg-yellow-500 disabled:bg-gray-300 disabled:cursor-not-allowed"
+              disabled={!userAddress || writeIsPending}
+            >
+              {buttonContent()}
+            </button>
+            {writeData?.transaction_hash && (
+              <a
+                href={`https://sepolia.voyager.online/tx/${writeData?.transaction_hash}`}
+                target="_blank"
+                className="block mt-2 text-blue-500 hover:text-blue-700 underline"
+                rel="noreferrer"
+              >
+                Check TX on Sepolia
+              </a>
+            )}
+          </form>
+          {/* <form className="bg-white p-4 border-black border">
+            <h3 className="text-lg font-bold mb-2">Write to Contract</h3>
+            <label htmlFor="amount" className="block text-sm font-medium text-gray-700">Amount:</label>
+            <input
+              type="number"
+              id="amount"
+              className="block w-full px-3 py-2 text-sm leading-6 border-black focus:outline-none focus:border-yellow-300 black-border-p"
+            />
+            <button
+              type="submit"
+              className="mt-3 border border-black text-black font-regular py-2 px-4 bg-yellow-300 hover:bg-yellow-500 disabled:bg-gray-300 disabled:cursor-not-allowed"
+            >
+              Send
+            </button>
+            <a
+              href={`https://sepolia.voyager.online/tx/`}
+              target="_blank"
+              className="block mt-2 text-blue-500 hover:text-blue-700 underline"
+              rel="noreferrer"
+            >
+              Check TX on Sepolia
+            </a>
+          </form> */}
+          {/* Step 4 --> Write to a contract -- End */}
 
         </div>
       </div>
