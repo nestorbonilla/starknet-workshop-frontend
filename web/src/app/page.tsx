35d3be90ea4fe7bab52b71a585e8b50a89828276
Nestor Bonilla <nestor.bonilla.s@gmail.com>
2024-09-11 17:23:33 -0700

    Read latest block data from blockchain

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,8 +1,17 @@
 'use client';
 import { FC } from 'react';
 import dynamic from 'next/dynamic';
+import { useBlockNumber } from '@starknet-react/core';
+import { BlockNumber } from "starknet";
 const WalletBar = dynamic(() => import('../components/WalletBar'), { ssr: false })
 const Page: FC = () => {
+
+  // Step 1 --> Read the latest block -- Start
+  const { data: blockNumberData, isLoading: blockNumberIsLoading, isError: blockNumberIsError } = useBlockNumber({
+    blockIdentifier: 'latest' as BlockNumber
+  });
+  const workshopEnds = 176000;
+  // Step 1 --> Read the latest block -- End
 
   return (
     <div className="min-h-screen bg-gray-100 p-4 flex flex-col">
@@ -11,10 +20,26 @@ const Page: FC = () => {
       <div className="flex flex-wrap justify-center gap-4">
 
         <div className="w-full max-w-md space-y-4">
+
           <div className="bg-white p-4 border-black border">
             <h2 className="text-xl font-bold mb-2">Wallet Connection</h2>
             <WalletBar />
           </div>
+
+          {/* Step 1 --> Read the latest block -- Start */}
+          {!blockNumberIsLoading && !blockNumberIsError && (
+            <div className={`p-4 border-black border ${blockNumberData! < workshopEnds ? "bg-green-500" : "bg-red-500"}`}>
+              <h3 className="text-lg font-bold mb-2">Read the Blockchain</h3>
+              <p>Current Block: {blockNumberData}</p>
+              <p>{blockNumberData! < workshopEnds ? "Workshop is live" : "Workshop has ended"}</p>
+            </div>
+          )}
+          {/* <div className={`p-4 border-black border`}>
+            <h3 className="text-lg font-bold mb-2">Read the Blockchain</h3>
+            <p>Current Block: 0</p>
+          </div> */}
+          {/* Step 1 --> Read the latest block -- End */}
+
         </div>
 
         <div className="w-full max-w-md space-y-4">
